923b4dd28bd722c3b86feedfa24ef27351b472e8
Baptiste Rosselet <baptiste.rosselet@protonmail.com>
2024-04-10 19:26:14 +0200

    implementing PING command response

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Uncomment this block to pass the first stage
+use anyhow::Result;
+use std::io::Write;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,12 +14,18 @@ fn main() {
     //
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                let _ = handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut conn: TcpStream) -> Result<()> {
+    conn.write_all(b"+PONG\r\n")?;
+    Ok(())
 }
