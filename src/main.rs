d4f83acb41b0900e7f71673f136e21656af1bdf2
Baptiste Rosselet <baptiste.rosselet@protonmail.com>
2024-04-10 20:55:14 +0200

    implementing multithreaded connectionsmanagement

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,18 @@ use anyhow::Result;
 use std::io::{Read, Write};
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    //
+
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 println!("accepted new connection");
-                let _ = handle_connection(stream);
+                thread::spawn(|| {
+                    let _ = handle_connection(stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
